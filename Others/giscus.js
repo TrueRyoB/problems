00502e121e2cc927bca390233606ef717bc255c8
RyoB <121699111+TrueRyoB@users.noreply.github.com>
2026-01-28 20:41:42 -0600

    Add giscus script for comments integration

---
+++ Others/giscus.js
@@ -0,0 +1,17 @@
+// To enable giscus (need to be on a website not on markdown display)
+<script src="https://giscus.app/client.js"
+        data-repo="TrueRyoB/problems"
+        data-repo-id="R_kgDOQ2ZMdA"
+        data-category="Giscus"
+        data-category-id="DIC_kwDOQ2ZMdM4C1kr2"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="ja"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
